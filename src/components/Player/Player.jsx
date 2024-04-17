b175bc7c563e17e6d64a45ea350b87b276333d0b
internet-unable <polocatic@gmail.com>
2024-04-17 17:38:59 +0300

    Update Player component
    Added dynamic content, dynamic btn caption and proper state update

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleBtnClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    let playerInput = <input type="text" value={name} required />;
+    let playerSpan = <span className="player-name">{name}</span>;
+    let playerContent = isEditing ? playerInput: playerSpan;
+    let btnCaption = isEditing ? 'Save' : 'Edit';
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerContent}
                 <span className="player-symbol">{symbol}</span>
             </span>
 
-            <button type="button">Edit</button>
+            <button type="button" onClick={handleBtnClick}>{btnCaption}</button>
         </li>
     );
 }
